a9526f9cedaf2a1d259866336493ce13c9b3d01c
Júlio Campos <jcserracampos@gmail.com>
2016-02-16 10:46:37 -0200

    Layout terminado. Faltando a implementacao do crawler e de relatorios.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    @publicacoes = Publicacao.find(Publicacao.last.id,Publicacao.last.id-1,Publicacao.last.id-2,Publicacao.last.id-3)
+    if Publicacao.last
+      @publicacoes = Publicacao.find(Publicacao.last.id, Publicacao.last.id-1, Publicacao.last.id-2, Publicacao.last.id-3)
+
+    else
+      @publicacoes = []
+
+    end
     @categorias = Categoria.all
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   resources :urls
   resources :hosts
+
+  resources :relatorios
+
   get 'welcome/index'
 
   resources :publicacoes do
